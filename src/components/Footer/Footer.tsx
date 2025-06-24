32ebaa4f807907a9286d4ddb7aba82dea137bc33
AbhaySemwal <shbhtsemwal@gmail.com>
2025-06-24 22:19:56 +0530

    footer fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="flex flex-col items-center gap-8 sm:gap-12 lg:gap-16 w-full">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8 lg:gap-12 mt-8 sm:mt-10 px-4 sm:px-8 lg:px-12 max-w-7xl mx-auto w-full">
-        <div className="flex flex-col w-full">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-4 lg:gap-12 mt-8 sm:mt-10 px-4 sm:px-8 lg:px-12 max-w-7xl mx-auto w-full">
+        <div className="flex flex-col w-full relative -top-10">
           <Image
             height={60}
             width={150}
@@ -40,11 +40,11 @@ const Footer = () => {
             <li>Cookie Policy</li>
           </ul>
         </div>
-        <div className="flex flex-col gap-4 sm:gap-5 lg:gap-6 w-full">
+        <div className="flex flex-col gap-4 sm:gap-3 w-full">
           <h1 className="font-semibold text-lg sm:text-xl lg:text-2xl leading-6 sm:leading-7 lg:leading-8">
             Social Links
           </h1>
-          <div className="flex gap-2 sm:gap-3 lg:gap-4">
+          <div className="flex gap-2 sm:gap-3 lg:gap-4 relative -left-2 sm:-left-4">
             {["fb.png", "ig.png", "x.png", "linkedin.png"].map((src, idx) => (
               <Image
                 key={idx}
@@ -52,7 +52,7 @@ const Footer = () => {
                 width={40}
                 alt={src.split(".")[0]}
                 src={`/${src}`}
-                className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12"
+                className="w-8 h-8 sm:w-14 sm:h-14"
               />
             ))}
           </div>
